8a5e28908692dd5a4ead14cc39d58e764698acd9
Steve J Nowak <sjnowak88@gmail.com>
2016-09-01 10:53:20 -0400

    Revised multithreading model of BasicScreen renderer. Replaced ForkAndJoin tasks with simpler Runnables. Makes for simpler shutdown.

--- rays2/src/org/snowjak/rays/ui/BasicScreen.java
+++ rays2/src/org/snowjak/rays/ui/BasicScreen.java
@@ -1,6 +1,8 @@
 package org.snowjak.rays.ui;
 
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
@@ -22,7 +24,9 @@ public class BasicScreen extends Screen {
 
 	private PixelWriter pixels;
 
-	private ForkJoinPool renderingThreadPool;
+	private ForkJoinPool renderingThreadForkJoinPool;
+
+	private ExecutorService renderingThreadPool;
 
 	/**
 	 * Create a new BasicScreen attached to the given {@link WritableImage}.
@@ -44,7 +48,8 @@ public class BasicScreen extends Screen {
 		super(camera, (int) image.getWidth() - 1, (int) image.getHeight() - 1);
 
 		this.pixels = image.getPixelWriter();
-		this.renderingThreadPool = new ForkJoinPool(4, ForkJoinPool.defaultForkJoinWorkerThreadFactory,
+		this.renderingThreadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1);
+		this.renderingThreadForkJoinPool = new ForkJoinPool(4, ForkJoinPool.defaultForkJoinWorkerThreadFactory,
 				(t, e) -> System.err.println("Problem in rendering thread [" + t.getName() + "]: " + e.getMessage()),
 				false);
 	}
@@ -52,21 +57,57 @@ public class BasicScreen extends Screen {
 	@Override
 	public void draw() {
 
-		renderingThreadPool.invoke(new RenderColumnTask(getScreenMinX(), getScreenMaxX()));
+		// renderingThreadForkJoinPool.invoke(new
+		// RenderColumnTask(getScreenMinX(), getScreenMaxX()));
+		for (int column = getScreenMinX(); column <= getScreenMaxX(); column++)
+			renderingThreadPool.submit(new ColumnRenderTask(column));
+
 	}
 
 	@Override
 	public void shutdown() {
 
 		super.shutdown();
-		System.out.println("Shutting down rendering tasks ...");
-		this.renderingThreadPool.shutdownNow();
+		if (!this.renderingThreadPool.shutdownNow().isEmpty()
+				|| !this.renderingThreadForkJoinPool.shutdownNow().isEmpty())
+			System.out.println("Shutting down rendering tasks ...");
 	}
 
 	@Override
 	public void drawPixel(int x, int y, RawColor color) {
 
 		Platform.runLater(() -> pixels.setColor(x, y, color.toColor()));
+	}
+
+	public class ColumnRenderTask implements Runnable {
+
+		private int column;
+
+		public ColumnRenderTask(int column) {
+			this.column = column;
+		}
+
+		@Override
+		public void run() {
+
+			try {
+				for (int y = getScreenMinY(); y <= getScreenMaxY(); y++) {
+					if (Thread.interrupted())
+						return;
+
+					Optional<RawColor> color = getCamera().shootRay(getCameraX(column), getCameraY(y));
+					if (color.isPresent())
+						drawPixel(column, getScreenMaxY() - y + getScreenMinY(), color.get());
+				}
+			} catch (Throwable t) {
+				System.err.println("Problem encountered in render-thread [" + Thread.currentThread().getName() + "]: "
+						+ t.getMessage());
+				t.printStackTrace(System.err);
+				return;
+			}
+
+		}
+
 	}
 
 	@SuppressWarnings("javadoc")
@@ -81,12 +122,12 @@ public class BasicScreen extends Screen {
 
 			if (endColumn - startColumn > 1) {
 				int midColumn = (endColumn - startColumn) / 2 + startColumn;
-				renderingThreadPool.invoke(new RenderColumnTask(startColumn, midColumn));
-				renderingThreadPool.invoke(new RenderColumnTask(midColumn + 1, endColumn));
+				renderingThreadForkJoinPool.invoke(new RenderColumnTask(startColumn, midColumn));
+				renderingThreadForkJoinPool.invoke(new RenderColumnTask(midColumn + 1, endColumn));
 
 			} else if (endColumn - startColumn == 1) {
-				renderingThreadPool.invoke(new RenderColumnTask(startColumn, startColumn));
-				renderingThreadPool.invoke(new RenderColumnTask(endColumn, endColumn));
+				renderingThreadForkJoinPool.invoke(new RenderColumnTask(startColumn, startColumn));
+				renderingThreadForkJoinPool.invoke(new RenderColumnTask(endColumn, endColumn));
 
 			} else {
 
