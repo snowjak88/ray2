ce02228ee655b224b6f6f75d56f77509a05b255d
Steve J Nowak <sjnowak88@gmail.com>
2016-09-01 16:24:14 -0400

    Added a new (extremely sparse) JavaDoc to PointLight

--- rays2/src/org/snowjak/rays/light/PointLight.java
+++ rays2/src/org/snowjak/rays/light/PointLight.java
@@ -5,12 +5,43 @@ import java.util.function.Function;
 import org.snowjak.rays.Ray;
 import org.snowjak.rays.color.RawColor;
 
+/**
+ * <p>
+ * PointLight represents light that comes from a particular, nearby location in
+ * space.
+ * </p>
+ * 
+ * @author rr247200
+ *
+ */
 public class PointLight extends Light {
 
+	/**
+	 * <p>
+	 * PointLight represents light that comes from a particular, nearby location
+	 * in space.
+	 * </p>
+	 * See {@link Light#Light(RawColor, RawColor, RawColor)}
+	 * 
+	 * @param ambientIntensity
+	 * @param diffuseIntensity
+	 * @param specularIntensity
+	 */
 	public PointLight(RawColor ambientIntensity, RawColor diffuseIntensity, RawColor specularIntensity) {
 		super(ambientIntensity, diffuseIntensity, specularIntensity);
 	}
 
+	/**
+	 * <p>
+	 * PointLight represents light that comes from a particular, nearby location
+	 * in space.
+	 * </p>
+	 * See {@link Light#Light(Function, Function, Function)}
+	 * 
+	 * @param ambientIntensityFunction
+	 * @param diffuseIntensityFunction
+	 * @param specularIntensityFunction
+	 */
 	public PointLight(Function<Ray, RawColor> ambientIntensityFunction,
 			Function<Ray, RawColor> diffuseIntensityFunction, Function<Ray, RawColor> specularIntensityFunction) {
 		super(ambientIntensityFunction, diffuseIntensityFunction, specularIntensityFunction);
