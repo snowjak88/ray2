b75509f792e6cdc3b698d83f80bcf083d68b2e8b
Steve J Nowak <sjnowak88@gmail.com>
2016-09-01 12:13:44 -0400

    Added the ability to save rendered images to the disk.
    Added a new context menu (used by right-clicking the rendered image).
    Using the "Save" control will activate a standard Save File file-picker,
    and allow you to specify the file-name, -location, and -type.
    
    TO DO Later:
     - programatically populate the list of usable image-formats in this
    file-picker
     - separate the image-saving function from the UI function

--- rays2/src/org/snowjak/rays/RaytracerApp.java
+++ rays2/src/org/snowjak/rays/RaytracerApp.java
@@ -1,6 +1,12 @@
 package org.snowjak.rays;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Executors;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageOutputStream;
 
 import org.snowjak.rays.camera.BasicCamera;
 import org.snowjak.rays.camera.Camera;
@@ -21,11 +27,19 @@ import org.snowjak.rays.ui.Screen;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 @SuppressWarnings("javadoc")
@@ -41,15 +55,69 @@ public class RaytracerApp extends Application {
 
 		World world = buildWorld();
 
-		WritableImage image = new WritableImage(800, 800);
+		WritableImage image = new WritableImage(320, 240);
 		Screen screen = new BasicScreen(image, world.getCamera());
 
 		ImageView imageView = new ImageView(image);
 		Group root = new Group(imageView);
 		Scene scene = new Scene(root, Color.BLACK);
 
+		ContextMenu imageContextMenu = new ContextMenu();
+		MenuItem saveImageMenuItem = new MenuItem("Save...");
+		saveImageMenuItem.setOnAction(e -> {
+			imageContextMenu.hide();
+
+			String[] knownImageFormatSuffixes = ImageIO.getWriterFileSuffixes();
+
+			int selectedIndex = 0;
+
+			FileChooser saveFileChooser = new FileChooser();
+			saveFileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+			saveFileChooser.setInitialFileName("image");
+
+			saveFileChooser.getExtensionFilters().add(new ExtensionFilter("png", "*.png"));
+			saveFileChooser.getExtensionFilters().add(new ExtensionFilter("jpeg", "*.jpg"));
+			saveFileChooser.getExtensionFilters().add(new ExtensionFilter("bmp", "*.bmp"));
+			saveFileChooser.setSelectedExtensionFilter(saveFileChooser.getExtensionFilters().get(selectedIndex));
+
+			File saveFile = saveFileChooser.showSaveDialog(primaryStage);
+
+			if (saveFile != null) {
+				String selectedImageFormatName = saveFileChooser.getSelectedExtensionFilter().getDescription();
+
+				BufferedImage bufferedImage = SwingFXUtils.fromFXImage(imageView.getImage(), null);
+				try {
+					ImageIO.write(bufferedImage, selectedImageFormatName, saveFile);
+
+				} catch (IOException e1) {
+					System.err.println("\nUnexpected problem saving the image to the file ["
+							+ saveFile.getAbsolutePath() + "]\nMessage: ");
+					System.err.println(e1.getMessage());
+					e1.printStackTrace(System.err);
+
+					Alert alertDialog = new Alert(AlertType.ERROR,
+							"Unexpected problem saving the image: " + e1.getMessage(), ButtonType.OK);
+
+					alertDialog.showAndWait();
+				}
+			}
+		});
+		imageContextMenu.getItems().add(saveImageMenuItem);
+
+		root.setOnMouseClicked(e -> {
+			switch (e.getButton()) {
+			case SECONDARY:
+				imageContextMenu.show(imageView, e.getScreenX(), e.getScreenY());
+				break;
+			default:
+				imageContextMenu.hide();
+				break;
+			}
+		});
+
 		primaryStage.setScene(scene);
 		primaryStage.setOnCloseRequest(e -> {
+			screen.shutdown();
 			Platform.exit();
 		});
 		primaryStage.show();
@@ -61,19 +129,17 @@ public class RaytracerApp extends Application {
 
 		World world = World.getSingleton();
 
-		for (int x = -6; x <= 6; x += 3) {
-			for (int y = 0; y <= 0; y += 2) {
-				for (int z = -6; z <= 6; z +=3) {
-					Sphere sphere = new Sphere(1d);
-					sphere.getTransformers().add(new Translation(x, y, z));
-					ColorScheme sphereColoring = new SimpleColorScheme(Color.hsb(360d * (x + z + 10d) / 20d, 1d, 1d));
-					sphere.setAmbientColorScheme(sphereColoring);
-					sphere.setDiffuseColorScheme(sphereColoring);
-					sphere.setSpecularColorScheme(new SimpleColorScheme(Color.WHITE));
-					sphere.setShininess(10d);
-					sphere.setReflectivity(0.9);
-					world.getShapes().add(sphere);
-				}
+		for (int x = -6; x <= 6; x += 2) {
+			for (int z = -6; z <= 6; z += 2) {
+				Sphere sphere = new Sphere(1d);
+				sphere.getTransformers().add(new Translation(x, 1d, z));
+				ColorScheme sphereColoring = new SimpleColorScheme(Color.hsb(360d * (x + z + 10d) / 20d, 1d, 1d));
+				sphere.setAmbientColorScheme(sphereColoring);
+				sphere.setDiffuseColorScheme(sphereColoring);
+				sphere.setSpecularColorScheme(new SimpleColorScheme(Color.WHITE));
+				sphere.setShininess(100d);
+				sphere.setReflectivity(0.75);
+				world.getShapes().add(sphere);
 			}
 		}
 
@@ -81,18 +147,21 @@ public class RaytracerApp extends Application {
 		ColorScheme planeColoring = new CheckerboardColorScheme(1d, Color.GREEN, Color.SADDLEBROWN);
 		plane.setAmbientColorScheme(planeColoring);
 		plane.setDiffuseColorScheme(planeColoring);
-		// world.getShapes().add(plane);
+		plane.setShininess(1e10);
+		plane.setReflectivity(0.75);
+		world.getShapes().add(plane);
 
-		Light pointLight;
-		pointLight = new PointLight(new RawColor(0.02, 0.02, 0.02), new RawColor(1d, 1d, 1d), new RawColor(1d, 1d, 1d));
-		pointLight.getTransformers().add(new Translation(0d, 5d, -6d));
-		world.getLights().add(pointLight);
+		for (int x = -6; x <= 6; x += 6) {
+			for (int z = -6; z <= 6; z += 6) {
+				Light pointLight;
+				pointLight = new PointLight(new RawColor(0.01, 0.01, 0.01), new RawColor(.3, .3, .3),
+						new RawColor(1d, 1d, 1d));
+				pointLight.getTransformers().add(new Translation(x, 5d, z));
+				world.getLights().add(pointLight);
+			}
+		}
 
-		pointLight = new PointLight(new RawColor(0.02, 0.02, 0.02), new RawColor(1d, 1d, 1d), new RawColor(1d, 1d, 1d));
-		pointLight.getTransformers().add(new Translation(0d, -5d, -6d));
-		world.getLights().add(pointLight);
-
-		Camera camera = new BasicCamera(2.0, 2.0, 45.0);
+		Camera camera = new BasicCamera(2.0, 45.0);
 		camera.getTransformers().add(new Translation(0d, 0d, -12d));
 		camera.getTransformers().add(new Rotation(-15d, 0d, 0d));
 		world.setCamera(camera);
