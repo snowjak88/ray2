ae15c8fee376c7f6ad25c625e4e2e356ba5de8eb
Steve J Nowak <sjnowak88@gmail.com>
2016-09-07 13:45:22 -0400

    Added different render-splitting options for multithreaded render
    MultithreadedScreenDecorator now supports 2 different methods for
    splitting the screen-render up among available threads:
     - column by column, spanning the entire screen
     - region by region

--- rays2/src/org/snowjak/rays/ui/MultithreadedScreenDecorator.java
+++ rays2/src/org/snowjak/rays/ui/MultithreadedScreenDecorator.java
@@ -16,6 +16,8 @@ public class MultithreadedScreenDecorator implements DrawsEntireScreen {
 
 	private ExecutorService renderingThreadPool;
 
+	private RenderSplitType splitType;
+
 	/**
 	 * Create a new {@link MultithreadedScreenDecorator} using the default
 	 * number of threads (i.e.,
@@ -36,15 +38,46 @@ public class MultithreadedScreenDecorator implements DrawsEntireScreen {
 	 * @param renderingThreadCount
 	 */
 	public MultithreadedScreenDecorator(DrawsScreenPixel child, int renderingThreadCount) {
+		this(child, renderingThreadCount, RenderSplitType.REGION);
+	}
+
+	/**
+	 * Create a new {@link MultithreadedScreenDecorator} using the specified
+	 * number of rendering threads and splitting the render-job using the
+	 * specified paradigm.
+	 * 
+	 * @param child
+	 * @param renderingThreadCount
+	 * @param splitType
+	 */
+	public MultithreadedScreenDecorator(DrawsScreenPixel child, int renderingThreadCount, RenderSplitType splitType) {
 		this.child = child;
 		this.renderingThreadPool = Executors.newFixedThreadPool(FastMath.max(renderingThreadCount, 1));
+		this.splitType = splitType;
 	}
 
 	@Override
 	public void draw() {
 
-		for (int column = child.getScreenMinX(); column <= child.getScreenMaxX(); column++)
-			renderingThreadPool.submit(new ColumnRenderTask(column));
+		switch (splitType) {
+		case COLUMN:
+			for (int column = child.getScreenMinX(); column <= child.getScreenMaxX(); column++)
+				renderingThreadPool.submit(new ColumnRenderTask(column));
+			break;
+
+		case REGION:
+			int sizeX = (child.getScreenMaxX() - child.getScreenMinX()) / 10;
+			int sizeY = (child.getScreenMaxY() - child.getScreenMinY()) / 10;
+
+			for (int startY = child.getScreenMinY(); startY < child.getScreenMaxY(); startY += sizeY + 1)
+				for (int startX = child.getScreenMinX(); startX < child.getScreenMaxX(); startX += sizeX + 1) {
+
+					int extentX = FastMath.min(startX + sizeX, child.getScreenMaxX());
+					int extentY = FastMath.min(startY + sizeY, child.getScreenMaxY());
+					renderingThreadPool
+							.submit(new RegionRenderTask(startX, startY, extentX - startX, extentY - startY));
+				}
+		}
 	}
 
 	@Override
@@ -53,6 +86,44 @@ public class MultithreadedScreenDecorator implements DrawsEntireScreen {
 		if (!this.renderingThreadPool.shutdownNow().isEmpty())
 			System.out.println("Shutting down rendering tasks ...");
 		child.shutdown();
+	}
+
+	@SuppressWarnings("javadoc")
+	public class RegionRenderTask implements Runnable {
+
+		private int startX, startY, sizeX, sizeY;
+
+		public RegionRenderTask(int startX, int startY, int sizeX, int sizeY) {
+			this.startX = startX;
+			this.startY = startY;
+			this.sizeX = sizeX;
+			this.sizeY = sizeY;
+		}
+
+		@Override
+		public void run() {
+
+			try {
+
+				for (int dx = 0; dx <= sizeX; dx++)
+					for (int dy = 0; dy <= sizeY; dy++) {
+
+						if (Thread.interrupted())
+							return;
+
+						Optional<RawColor> color = child.getRayColor(startX + dx, startY + dy);
+						if (color.isPresent())
+							child.drawPixel(startX + dx, startY + dy, color.get());
+
+					}
+
+			} catch (Throwable t) {
+				System.err.println("Problem encountered in render-thread [" + Thread.currentThread().getName() + "]: "
+						+ t.getMessage());
+				t.printStackTrace(System.err);
+				return;
+			}
+		}
 	}
 
 	@SuppressWarnings("javadoc")
@@ -86,6 +157,24 @@ public class MultithreadedScreenDecorator implements DrawsEntireScreen {
 
 		}
 
+	}
+
+	/**
+	 * Denotes how a {@link MultithreadedScreenDecorator} will split up the
+	 * screen-rendering job.
+	 * 
+	 * @author rr247200
+	 *
+	 */
+	public static enum RenderSplitType {
+		/**
+		 * Split up the screen into individual regions.
+		 */
+		REGION,
+		/**
+		 * Render the entire screen column by column.
+		 */
+		COLUMN;
 	}
 
 }
