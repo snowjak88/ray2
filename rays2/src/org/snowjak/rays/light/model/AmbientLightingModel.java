8def4f5992c978c729e55e4b16fe130d8c03cfdc
Steve J Nowak <sjnowak88@gmail.com>
2016-09-14 16:20:47 -0400

    New LightingModels, intended to be composited

---
+++ rays2/src/org/snowjak/rays/light/model/AmbientLightingModel.java
@@ -0,0 +1,56 @@
+package org.snowjak.rays.light.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.snowjak.rays.Ray;
+import org.snowjak.rays.World;
+import org.snowjak.rays.color.RawColor;
+import org.snowjak.rays.intersect.Intersection;
+import org.snowjak.rays.light.Light;
+import org.snowjak.rays.shape.Shape;
+
+/**
+ * Implements an ambient lighting model.
+ * <p>
+ * Simply, all points are illuminated by the sum of every light's
+ * ambient-intensity.
+ * </p>
+ * 
+ * @author snowjak
+ *
+ */
+public class AmbientLightingModel implements LightingModel {
+
+	@Override
+	public Optional<RawColor> determineRayColor(Ray ray, List<Intersection<Shape>> intersections) {
+
+		if (intersections.isEmpty())
+			return Optional.empty();
+
+		return Optional.of(lightIntersection(intersections.stream()
+				.filter(i -> Double.compare(i.getDistanceFromRayOrigin(), World.DOUBLE_ERROR) >= 0)
+				.findFirst()
+				.get()));
+	}
+
+	private RawColor lightIntersection(Intersection<Shape> intersection) {
+
+		Vector3D point = intersection.getPoint();
+		RawColor totalLightAtPoint = new RawColor();
+
+		for (Light light : World.getSingleton().getLights()) {
+
+			Ray toLightRay = new Ray(point, light.getLocation().subtract(point));
+
+			totalLightAtPoint = totalLightAtPoint.add(light.getAmbientIntensity(toLightRay));
+
+		}
+
+		RawColor pointColor = intersection.getEnteringMaterial().getColor(point);
+
+		return totalLightAtPoint.multiply(pointColor);
+	}
+
+}
---
+++ rays2/src/org/snowjak/rays/light/model/LambertianDiffuseLightingModel.java
@@ -0,0 +1,65 @@
+package org.snowjak.rays.light.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.snowjak.rays.Ray;
+import org.snowjak.rays.World;
+import org.snowjak.rays.color.RawColor;
+import org.snowjak.rays.intersect.Intersection;
+import org.snowjak.rays.light.Light;
+import org.snowjak.rays.shape.Shape;
+
+/**
+ * Implements the Lambertian diffuse lighting model.
+ * 
+ * @author snowjak88
+ *
+ */
+public class LambertianDiffuseLightingModel implements LightingModel {
+
+	@Override
+	public Optional<RawColor> determineRayColor(Ray ray, List<Intersection<Shape>> intersections) {
+
+		if (intersections.isEmpty())
+			return Optional.empty();
+
+		return Optional.of(lightIntersection(intersections.stream()
+				.filter(i -> Double.compare(i.getDistanceFromRayOrigin(), World.DOUBLE_ERROR) >= 0)
+				.findFirst()
+				.get()));
+	}
+
+	private RawColor lightIntersection(Intersection<Shape> intersection) {
+
+		Vector3D point = intersection.getPoint();
+		Vector3D normal = intersection.getNormal();
+		RawColor totalLightAtPoint = new RawColor();
+
+		for (Light light : World.getSingleton().getLights()) {
+
+			Ray toLightRay = new Ray(point, light.getLocation().subtract(point));
+
+			boolean isOccludingIntersections = World.getSingleton()
+					.getShapeIntersections(toLightRay)
+					.parallelStream()
+					.anyMatch(i -> Double.compare(i.getDistanceFromRayOrigin(), World.DOUBLE_ERROR) >= 0);
+			if (isOccludingIntersections)
+				continue;
+
+			double exposure = light.getExposure(point, normal);
+
+			if (Double.compare(exposure, 0d) <= 0)
+				continue;
+
+			totalLightAtPoint = totalLightAtPoint.add(light.getDiffuseIntensity(toLightRay).multiplyScalar(exposure));
+
+		}
+
+		RawColor pointColor = intersection.getEnteringMaterial().getColor(point);
+
+		return totalLightAtPoint.multiply(pointColor);
+	}
+
+}
